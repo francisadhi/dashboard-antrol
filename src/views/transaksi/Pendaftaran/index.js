b455ddf797862fb8add78e4f08222f3b542efc4e
adhi <francis.adhi@gmail.com>
2022-02-08 14:18:32 +0700

    add booking list , reg

--- src/views/transaksi/Pendaftaran/index.js
+++ src/views/transaksi/Pendaftaran/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
+  CButton,
   CCard,
   CCardBody,
   CCardHeader,
@@ -14,24 +15,95 @@ import {
   CTableRow,
 } from '@coreui/react'
 import { DocsCallout, DocsExample } from 'src/components'
+import { bookingActions } from '../../../_actions'
+import { connect } from 'react-redux'
+import axios from 'axios'
 
-const ListPendaftaran = () => {
-  return (
-    <CRow>
-      {/* <CCol xs={12}>
-        <DocsCallout name="Table" href="components/table" />
-      </CCol> */}
-      <CCol xs={12}>
-        <CCard className="mb-4">
-          <CCardHeader>
-            <h1>
-              <strong>Coming</strong> <small>Soon</small>
-            </h1>
-          </CCardHeader>
-        </CCard>
-      </CCol>
-    </CRow>
-  )
+class ListPendaftaran extends Component {
+  // constructor(props) {
+  //   super(props)
+
+  //   this.props.getAll()
+  // }
+
+  constructor() {
+    super()
+    this.state = {
+      bookings: [],
+    }
+  }
+
+  componentDidMount() {
+    setInterval(
+      axios
+        .get(`http://36.92.135.163:3001/api/bookings`)
+        // API.get(`users/`)
+        .then((res) => {
+          const bookings = res.data
+          this.setState({ bookings })
+          console.log(bookings)
+        }),
+      1000,
+    )
+    // window.location.reload(true)
+  }
+
+  render() {
+    //const { booking } = this.props
+    //console.log(bookings)
+    return (
+      <CRow>
+        <CCol xs={12}>
+          <CCard className="mb-4">
+            <CCardHeader>
+              <strong>List Pendaftaran Pasien</strong> <small></small>
+            </CCardHeader>
+            <CCardBody>
+              <CTable>
+                <CTableHead>
+                  <CTableRow>
+                    <CTableHeaderCell scope="col">No REGISTRASI</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">NO RM</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">TANGGAL</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">NAMA</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">ALAMAT</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">STATUS</CTableHeaderCell>
+                  </CTableRow>
+                </CTableHead>
+                <CTableBody>
+                  {this.state.bookings.map((booking) => (
+                    <CTableRow key={booking.FS_KD_TRS}>
+                      <CTableDataCell></CTableDataCell>
+                      <CTableDataCell>{booking.FS_MR}</CTableDataCell>
+                      <CTableDataCell>{booking.FD_TGL_TRS}</CTableDataCell>
+                      <CTableDataCell>{booking.FS_NM_PASIEN_BOOK}</CTableDataCell>
+                      <CTableDataCell>{booking.FS_ALM_PASIEN_BOOK}</CTableDataCell>
+                      <CTableDataCell>
+                        <CButton color="success" shape="rounded-pill">
+                          Selesai
+                        </CButton>
+                      </CTableDataCell>
+                    </CTableRow>
+                  ))}
+                </CTableBody>
+              </CTable>
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    )
+  }
 }
 
+// function mapState(state) {
+//   const { booking } = state
+//   console.log(booking)
+//   return { booking }
+// }
+
+// const actionCreators = {
+//   getAll: bookingActions.getAll,
+// }
+
+//export default ListBooking = connect(mapState, actionCreators)(ListBooking)
 export default ListPendaftaran
