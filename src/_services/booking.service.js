9ea19e43529794f05a269ddd31dca51dc5d8e5cf
adhi <francis.adhi@gmail.com>
2022-02-08 12:56:25 +0700

    fetch to ip public

--- src/_services/booking.service.js
+++ src/_services/booking.service.js
@@ -12,7 +12,7 @@ function getAll() {
   }
 
   // return fetch(`http://36.92.135.163:3001/api/bookings`, requestOptions).then(handleResponse)
-  return fetch(`http://192.168.0.154:3001/api/bookings`, requestOptions).then(handleResponse)
+  return fetch(`http://36.92.135.163:3001/api/bookings`, requestOptions).then(handleResponse)
 }
 
 function handleResponse(response) {
--- src/_services/user.service.js
+++ src/_services/user.service.js
@@ -19,7 +19,7 @@ async function login(username, password) {
   }
 
   // const response = await fetch(`http://36.92.135.163:3001/api/login2`, requestOptions)
-  const response = await fetch(`http://192.168.0.154:3001/api/login2`, requestOptions)
+  const response = await fetch(`http://36.92.135.163:3001/api/login2`, requestOptions)
   const user = await handleResponse(response)
   // store user details and jwt token in local storage to keep user logged in between page refreshes
   localStorage.setItem('user', JSON.stringify(user.response.user))
